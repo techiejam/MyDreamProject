746d9ffc338c8b01b87af260317c787095fc51a2
techiejam <78722286+techiejam@users.noreply.github.com>
2023-12-20 18:57:47 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,10 +1,8 @@
  class add{
     public static void main(String[] args) {
-        int a,b,c,d;
-        a=10;b=10;
-        c=a+b;
-        d=a-b;
+         int a=10,b=10;
+        int c=a+b;
+    
         System.out.println("the output is:"+c);
-        System.out.println("the output is :"+d);
     }
 }
