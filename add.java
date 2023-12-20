a18514e85ccbb41afc503bfbf0e70e93f9fb1a54
techiejam <78722286+techiejam@users.noreply.github.com>
2023-12-20 18:52:23 +0530

    Int changed with variables

--- add.java
+++ add.java
@@ -1,7 +1,10 @@
  class add{
     public static void main(String[] args) {
-        int a=10,b=10;
-        int c=a+b;
+        int a,b,c,d;
+        a=10;b=10;
+        c=a+b;
+        d=a-b;
         System.out.println("the output is:"+c);
+        System.out.println("the output is :"+d);
     }
 }
